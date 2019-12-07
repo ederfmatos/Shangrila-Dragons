0505ea5ba846ad369d4f17a0f2aae9bd0e0ed729
ederfmatos <ederfmatos@gmail.com>
2019-12-07 11:46:18 -0300

    Create Medium and Hard Levels

--- app/src/main/java/com/spectron/dragoesdeshangrila/levels/HardLevel.java
+++ app/src/main/java/com/spectron/dragoesdeshangrila/levels/HardLevel.java
@@ -2,23 +2,24 @@ package com.spectron.dragoesdeshangrila.levels;
 
 import com.spectron.dragoesdeshangrila.interfaces.LevelOptionsInterface;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class HardLevel implements LevelOptionsInterface {
 
     @Override
     public List<Integer> whenRemoveOneDragon() {
-        return null;
+        return Arrays.asList(1, 5, 9, 13);
     }
 
     @Override
     public List<Integer> whenRemoveTwoDragons() {
-        return null;
+        return Arrays.asList(2, 6, 10, 14);
     }
 
     @Override
     public List<Integer> whenRemoveThreeDragons() {
-        return null;
+        return Arrays.asList(3, 7, 11);
     }
 
 }
--- app/src/main/java/com/spectron/dragoesdeshangrila/levels/MediumLevel.java
+++ app/src/main/java/com/spectron/dragoesdeshangrila/levels/MediumLevel.java
@@ -2,23 +2,24 @@ package com.spectron.dragoesdeshangrila.levels;
 
 import com.spectron.dragoesdeshangrila.interfaces.LevelOptionsInterface;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class MediumLevel implements LevelOptionsInterface {
 
     @Override
     public List<Integer> whenRemoveOneDragon() {
-        return null;
+        return Arrays.asList(1, 5, 9);
     }
 
     @Override
     public List<Integer> whenRemoveTwoDragons() {
-        return null;
+        return Arrays.asList(2, 6, 10);
     }
 
     @Override
     public List<Integer> whenRemoveThreeDragons() {
-        return null;
+        return Arrays.asList(3, 7, 11);
     }
 
 }
